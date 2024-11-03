d6907213dd2069faed149876715213ced6307182
Francisco Bravo <fcobravo.23@gmail.com>
2024-11-03 20:48:08 -0300

    se agrego una carpeta JS

---
+++ js/variables.js
@@ -0,0 +1,21 @@
+/***
+ * Variables
+ * string cadena
+ * number numero
+ * boolean true o false
+ * object objtos
+ * function
+ *
+ * null- valor nuloññ
+ * undefined - valor sin definir
+ */
+
+const resultado = 4 + 4;
+const nombre = "francisco";
+const apellido = "bravo";
+const resultado1 = nombre + " " + apellido;
+
+let mivariable = "texto";
+mivariable = 10;
+console.log(typeof resultado1);
+console.log(typeof mivariable);
