ab8f6cafd9d77c0ffd1e6c5161fe659fbd0473cd
Francisco Bravo <fcobravo.23@gmail.com>
2024-11-06 20:14:13 -0300

    dia3 aprendiendo programacion

--- js/dia02Operadores/Operator.js
+++ js/dia02Operadores/Operator.js
@@ -1,8 +1,43 @@
-let a = 3
-let x = (100 + 50) * a;
+let ax = 3;
+let x = (100 + 50) * ax;
 console.log(x);
 
 let xx = 10;
 xx += 5;
 console.log(xx);
 
+//ejercicios
+let a = 10;
+let b = 5;
+let resultado = a + b; // 15
+console.log(resultado);
+
+let nombre = "Juan";
+let edad = 25;
+let mensaje = nombre + " tiene " + edad + " años.";
+console.log(mensaje); // "Juan tiene 25 años."
+
+let a2 = 10;
+let b2 = 3;
+let resultado2 = a2 - b2; // 7
+console.log(resultado);
+
+let a3 = 4;
+let b3 = 5;
+let resultado3 = a * b; // 20
+console.log(resultado3);
+
+let a4 = 10;
+let b4 = 3;
+let resultado4 = a4 % b4; // 1 (porque 10 dividido entre 3 da 3 con un residuo de 1)
+console.log(resultado4);
+
+let a5 = 5;
+let resultado5 = a5++; // Se asigna a resultado el valor de a, luego se incrementa
+console.log(a5); // 6
+console.log(resultado5); // 5
+
+let a6 = 5;
+let resultado6 = --a; // a se decrementa primero, luego se asigna a resultado
+console.log(a6); // 4
+console.log(resultado6); // 4
