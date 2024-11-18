333255d90eb06b8ca4fb20163e7ab044c40a3066
Francisco Bravo <fcobravo.23@gmail.com>
2024-11-18 19:02:15 -0300

    dia10 programacion

--- js/dia08-DOM/DOM.js
+++ js/dia08-DOM/DOM.js
@@ -9,3 +9,47 @@ let boton = document.getElementById("btn-cambiar");
 boton.addEventListener("click", function () {
   titulo.textContent = "¡Título cambiado!";
 });
+
+/**
+ * Manipulacion los estilos css
+ * puedes cambiar los estilos de los
+ * elementos directamente desde javascript usando la propiedad syle
+ */
+let cuadro = document.getElementById("cuadro");
+let boton2 = document.getElementById("btn-cambiar-color");
+
+boton2.addEventListener("click", () => {
+  // Cambiar color de fondo
+  cuadro.style.backgroundColor = "blue";
+
+  // Cambiar tamaño del cuadro
+  cuadro.style.width = "200px";
+  cuadro.style.height = "200px";
+});
+
+/**
+ * Creando nuevos elementos
+ * Puedes crear nuevos elementos HTML y agregarles a la página de manera dinámica.
+ */
+let lista = document.getElementById("lista");
+let boton3 = document.getElementById("btn-agregar");
+
+boton3.addEventListener("click", () => {
+  let nuevoElemento = document.createElement("li");
+  nuevoElemento.textContent = "Elemento nuevo";
+
+  //agregar el nuevo li al final de la lista
+  lista.appendChild(nuevoElemento);
+});
+
+/**
+ * 5. Eliminando elementos
+Puedes eliminar elementos del DOM utilizando el método removeChild().
+ */
+let = boton4 = document.getElementById("btn-eliminar");
+
+boton4.addEventListener("click", () => {
+  let primerElementDelete = lista.firstElementChild; //obteniene el primer elemtno de la lista
+  lista.removeChild(primerElementDelete);
+  console.log(lista);
+});
